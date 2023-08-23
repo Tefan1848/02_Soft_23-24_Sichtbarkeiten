d0cec996d5bba1127df99554f43497e3bf2a9341
Stefan <stgrybo4@web.de>
2023-08-23 10:18:07 +0200

    Implementation der Aufgabe Seriennummer

---
+++ 02_Seriennummer.py
@@ -0,0 +1,16 @@
+class ElektronicProduct:
+    def __init__(self):
+        self.serial_number = 0
+
+    def __generate_serial_number(self, serial_number):
+        if 0 < serial_number <= 1000:
+            self.serial_number = serial_number
+
+    def get_serial_number(self):
+        return self.serial_number
+
+my_product = ElektronicProduct()
+my_product.generate_serial_number(1)
+print(my_product.get_serial_number())
+my_product.generate_serial_number(2)
+print(my_product.get_serial_number())
